80f0836dea077c921b849ab0ae74a6ecbae6d6ed
RicardoCalvoP <a01028889@tec.mx>
2024-09-11 12:14:54 -0600

    implemented first endpoint to call users

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import mongoose, { connect } from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Importar el paquete 'cors'
 import { User, Employee } from './Models/models.js';
-
-
-
-
 
 dotenv.config();
 
 const app = express();
-//app.use(cors());
+
+// Habilitar CORS para todas las solicitudes
+app.use(cors({ // Permite que cualquier origen pueda acceder a la API
+    exposedHeaders: ['X-Total-Count'], // Expone el encabezado X-Total-Count
+}));
+
 app.use(express.json());
 
 
@@ -33,8 +35,17 @@ async function connectDB() {
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
-        res.json(users);
-        console.log(users);
+        const usersWithId = users.map(users => ({
+            id: users._id, // Transformar _id a id para React-Admin
+            name: users.name,
+            surname: users.surname,
+            email: users.email,
+            phone: users.phone,
+            num_donations: users.num_donations
+        }));
+        res.set('X-Total-Count', users.length);  // Estima el tamaño de los resultados y lo agrega a X-Total-Count en el envabezado de la respuesta para que React-Admin pueda hacer una asignación correcta
+        console.log(usersWithId);
+        res.json(usersWithId);
     } catch (error) {
         res.status(500).send('Error fetching users');
     }
