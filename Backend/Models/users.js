9a181c125a179f03e50cbc69ddc955db42df36a1
RicardoCalvoP <a01028889@tec.mx>
2024-09-11 17:29:31 -0600

    changed unique element in phone

--- Backend/Models/users.js
+++ Backend/Models/users.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     surname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: {
-        type: String, required: false, unique: true
+        type: String, required: false, default: "NA"
     },
     num_donations: { type: Number, default: 0 }
 });
