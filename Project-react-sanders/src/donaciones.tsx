ec3b90db68e88f0600d0bbd1c94a9ef38d5f5101
RicardoCalvoP <a01028889@tec.mx>
2024-09-13 22:19:27 -0600

    added labels and added create function

--- Project-react-sanders/src/donaciones.tsx
+++ Project-react-sanders/src/donaciones.tsx
@@ -1,7 +1,7 @@
 import { useMediaQuery, Theme } from "@mui/material";
 import { create } from "domain";
 import {
-    List, Datagrid, TextField, ReferenceField, DateField,
+    List, Datagrid, TextField, ReferenceField, DateField, NumberField, EmailField,
     SimpleForm, TextInput, NumberInput, DateInput,
     Create
 } from "react-admin";
@@ -17,24 +17,31 @@ export const DonationList = () => {
         <List>
             <Datagrid>
                 <TextField source="id" />
-                <ReferenceField source=" userId" reference="users" />
-                <TextField source="programa" />
-                <TextField source="monto" />
-                <DateField source="fecha" />
+                <TextField source="donator_name" label="Nombre" />
+                <TextField source="donator_surname" label="Apellido" />
+                <EmailField source="donator_email" label="Email" />
+                <TextField source="donator_phone" label="Telefono" />
+                <TextField source="comment" label="Comentario" />
+                <NumberField source="amount" label="Monto" />
+                <DateField source="date" label="Fecha" />
             </Datagrid>
+
         </List>
     )
 }
 
-export const DonationCreate = () => {
+export const DonationCreate = (props: any) => {
     return (
-        <Create>
+        <Create {...props}>
             <SimpleForm>
-                <TextInput source="programa" />
-                <NumberInput source="monto" />
-                <TextInput source="email" />
-                <DateInput source="fecha" />
+                <TextInput source="donator_name" label="Nombre" />
+                <TextInput source="donator_surname" label="Apellido" />
+                <TextInput source="donator_email" label="Email" />
+                <TextInput source="donator_phone" label="Telefono" />
+                <TextInput source="comment" label="Comentario" />
+                <NumberInput source="amount" label="Monto" />
             </SimpleForm>
         </Create>
+
     )
 }
