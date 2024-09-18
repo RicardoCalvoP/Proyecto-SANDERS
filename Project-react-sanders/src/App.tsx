66f90d79b8758a49dc14df4e20b13d672f69a544
RicardoCalvoP <a01028889@tec.mx>
2024-09-18 17:31:47 -0600

    start creating auth

--- Project-react-sanders/src/App.tsx
+++ Project-react-sanders/src/App.tsx
@@ -1,10 +1,6 @@
-import * as React from "react";
-import { Layout } from "./Layout";
-import LoginPage from "./loginpage";
-import MyLayout from "./MyLayout";
+import React, { useState, useEffect } from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from 'ra-data-json-server';
-const dataProvider = jsonServerProvider('http://localhost:5001');
 
 // Imported Icons
 import PeopleIcon from '@mui/icons-material/People';
@@ -16,40 +12,46 @@ import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 import { UserList, UsersCreate } from "./users";
 import { DonationCreate, DonationList } from "./donaciones";
 import { EmployeeCreate, EmployeeList } from "./empleados";
-
-// Extra libraries 
-import CIcon from '@coreui/icons-react';
 import { StatsList } from "./estadisticas";
-import { json } from "stream/consumers";
+import LoginPage from "./loginpage";
+import MyLayout from "./MyLayout";
+import { i18nProvider } from "./i18nProvider";
 
-
+// Data Provider
+const dataProvider = jsonServerProvider('http://localhost:5001');
 
 export const App = () => (
   <Admin
-    loginPage={LoginPage} layout={MyLayout} dataProvider={dataProvider}
-  // layout={Layout}   
+    loginPage={LoginPage}
+    layout={MyLayout}
+    dataProvider={dataProvider}
+    i18nProvider={i18nProvider}
   >
     <Resource
-      name="users"
+      name="usuarios"
       list={UserList}
       create={UsersCreate}
-      icon={PeopleIcon} />
-
+      icon={PeopleIcon}
+    />
     <Resource
-      name="donations"
+      name="donaciones"
       list={DonationList}
       create={DonationCreate}
-      icon={PaidOutlinedIcon} />
-
+      icon={PaidOutlinedIcon}
+    />
     <Resource
       name="estadisticas"
       list={StatsList}
-      icon={BarChartOutlinedIcon} />
-
+      icon={BarChartOutlinedIcon}
+    />
     <Resource
-      name="employees"
+      name="empleados"
       list={EmployeeList}
       create={EmployeeCreate}
-      icon={BadgeOutlinedIcon} />
+      icon={BadgeOutlinedIcon}
+    />
   </Admin>
 );
+
+
+export default App;
