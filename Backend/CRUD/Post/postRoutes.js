302761253c051eb397ea5adec5a47c233c2c74c5
RicardoCalvoP <a01028889@tec.mx>
2024-09-28 22:54:02 -0600

    mail files

--- Backend/CRUD/Post/postRoutes.js
+++ Backend/CRUD/Post/postRoutes.js
@@ -3,6 +3,7 @@ import postDonations from './postDonation.js';
 import postEmployees from './postEmployees.js';
 import postUsers from './postUsers.js'
 import postLogin from '../../LoginBackend/login.js';
+import postEmail from '../../MailSender/postMail.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use(postDonations);
 router.use(postEmployees);
 router.use(postUsers);
 router.use(postLogin);
+router.use(postEmail);
 
 export default router;
