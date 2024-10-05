75a85ee2c5655792190614cf5a21913d8b38a304
RicardoCalvoP <a01028889@tec.mx>
2024-10-04 23:12:19 -0600

    empty admin dashboard

--- Project-react-sanders/src/Dashboards/adminDashboard.tsx
+++ Project-react-sanders/src/Dashboards/adminDashboard.tsx
@@ -1,41 +1,11 @@
-// Admin Dashboard
-import * as React from 'react';
-import { Resource } from "react-admin"; // Elimina Admin de aquí
-import { UserList } from '../Pages/users';
-import { DonationList, SANDERSDonationCreate } from '../Pages/donaciones';
-import { StatsList } from '../Pages/estadisticas';
-import { EmployeeList, EmployeeCreate } from '../Pages/empleados';
+import React from 'react';
 
-import { PeopleIcon, PaidOutlinedIcon, BadgeOutlinedIcon, BarChartOutlinedIcon } from '../Components/icons';
-
-const AdminDashboard = () => {
+const DashboardAdmin = () => {
     return (
-        <>
-            <Resource
-                name="donaciones"
-                list={DonationList}
-                create={SANDERSDonationCreate} // Creation of donation for employees
-                icon={PaidOutlinedIcon}
-            />
-            <Resource
-                name="usuarios"
-                list={UserList}
-                // create={UsersCreate} user are created when donating
-                icon={PeopleIcon}
-            />
-            <Resource
-                name="estadisticas"
-                list={StatsList}
-                icon={BarChartOutlinedIcon}
-            />
-            <Resource
-                name="empleados"
-                list={EmployeeList}
-                create={EmployeeCreate}
-                icon={BadgeOutlinedIcon}
-            />
-        </>
+        <div>
+            <h1>Panel de Administración</h1>
+        </div>
     );
 };
 
-export default AdminDashboard;
+export default DashboardAdmin;
