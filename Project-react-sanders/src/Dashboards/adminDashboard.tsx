a95c4cf4fcad64ae6049421d5419fddf8f906e9e
RicardoCalvoP <a01028889@tec.mx>
2024-10-11 18:35:48 -0600

    added role check before showing info

--- Project-react-sanders/src/Dashboards/adminDashboard.tsx
+++ Project-react-sanders/src/Dashboards/adminDashboard.tsx
@@ -7,32 +7,42 @@ import TopDonorsByCount from '../Pages/Charts/topDonatorsByCount';
 import TopDonorsByAmount from '../Pages/Charts/topDonatorsByAmount';
 
 const Overview = () => {
-    return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', textAlign: 'center', justifyContent: 'space-around', marginTop: -10 }}>
+    const identity = JSON.parse(localStorage.getItem('identity') || '{}');
+    const userRol = identity.rol;
+    if (userRol === "Admin") {
+        return (
 
 
-            <Box sx={{ flex: 1, minWidth: '40%' }}>
-                <LastDonations />
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', textAlign: 'center', justifyContent: 'space-around', marginTop: -10 }}>
+
+
+
+                <Box sx={{ flex: 1, minWidth: '40%' }}>
+                    <LastDonations />
+                </Box>
+                <Box sx={{ flex: 1, minWidth: '40%' }}>
+                    <TypeDonation />
+                </Box>
+
+                <Box sx={{ flex: 1, minWidth: '70%' }}>
+                    <WeekChart />
+                </Box>
+
+
+                <Box sx={{ flex: 1, minWidth: '40%' }}>
+                    <TopDonorsByAmount />
+                </Box>
+                <Box sx={{ flex: 1, minWidth: '40%' }}>
+                    <TopDonorsByCount />
+                </Box>
             </Box>
-            <Box sx={{ flex: 1, minWidth: '40%' }}>
-                <TypeDonation />
-            </Box>
-
-            <Box sx={{ flex: 1, minWidth: '70%' }}>
-                <WeekChart />
-            </Box>
 
 
-            <Box sx={{ flex: 1, minWidth: '40%' }}>
-                <TopDonorsByAmount />
-            </Box>
-            <Box sx={{ flex: 1, minWidth: '40%' }}>
-                <TopDonorsByCount />
-            </Box>
-        </Box>
-
-
-    );
+        );
+    }
+    else {
+        return (<Box><h2>Error 401</h2></Box>);
+    }
 };
 
 export default Overview;
